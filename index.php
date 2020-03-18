6a5001b45c4813efb95380d33776bf80cf3e6dc7
tracks12 <anarchy.dark97@gmail.com>
2020-03-18 19:08:04 +0100

    remove parazit file and optimize code, changing border graduation

--- index.php
+++ index.php
@@ -46,7 +46,29 @@
 	</head>
 	<div id="particles-js"></div>
 	<body onload="anim.startTime();">
-		<?php require("./nav.php"); ?>
+		<nav>
+			<p id="time">00:00:00</p>
+			<div>
+				<span class="fa fa-bars"></span>
+			</div>
+			<ul>
+				<li><a href="./#me">moi</a></li>
+				<li><a href="./#skills">compétences</a></li>
+				<li><a href="./#xp">parcours pro</a></li>
+				<li><a href="./#grad">formations</a></li>
+				<li>
+					<a href="#portfolio">portfolio <span class="fa fa-sort-desc"></span></a>
+					<ul>
+						<?php
+							for($i = 0; $i < count($cat); $i++)
+								if($cat[$i])
+									echo("<li><a href=\"#portfolio-{$cat[$i]}\">{$cat[$i]}</a></li>");
+						?>
+					</ul>
+				</li>
+				<li><a href="#contact">contact</a></li>
+			</ul>
+		</nav>
 		<a id="upper" href="#me">
 			<span class="fa fa-chevron-up"></span>
 		</a>
@@ -260,7 +282,12 @@
 				</article>
 			</aside>
 		</section>
-		<?php require("./footer.html"); ?>
+		<footer>
+			<a href="#me">
+				<span class="fa fa-chevron-up"></span>
+			</a>
+			<p>Fait par Cardinal Florian</p>
+		</footer>
 	</body>
 </html>
 <!-- END -->
