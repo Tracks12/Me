9c04872eaa0cd1191bc265a52a1c534ba4fd3624
tracks12 <anarchy.dark97@gmail.com>
2020-03-22 18:21:47 +0100

    Change comment and explode depencies from main js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
-/********************************
-*                               *
-*   Auteur : CARDINAL Florian   *
-*   Date   : 22/12/2018 17:19   *
-*   Page   : script.js          *
-*                               *
-********************************/
+/**
+ * Auteur : CARDINAL Florian
+ * Date   : 22/12/2018 17:19
+ * Page   : script.js
+ */
 
 class anim {
 	constructor() {
@@ -72,46 +70,6 @@ class anim {
 		$('#time').html(h+sep+m+s);
 		setTimeout(() => anim.startTime(sep), delay);
 	}
-}
-
-function credit() {
-	let output = "";
-	let content = {
-		msg: Array(
-			"Développeur Web Junior full stack\n",
-			"---------------------------------\n\n",
-			"Front-end skills: Html5 / Css3 / Js / Twig\n",
-			"Back-end skills: Php7 / Sql / MySQL / Python\n\n",
-			"Front-end technology: Angular 8, Bootstrap (Responsive Design) & JQuery\n",
-			"Back-end technology: Symfony, Doctrine, Fixture, phpBB, PhpMyAdmin & NodeJS\n\n",
-			"Mastery of Windows & Linux environments,\n",
-			"Favorite IDEs: Atom, VSCode.\n\n",
-			"Preferably opts for the Agile method with a Git environment to work efficiently and optimally.\n\n",
-			"Very good team spirit, attentive to details and cooperative, ensuring total success for any web project.\n\n",
-			"Website developed by my own !\n\n"
-		),
-		pageInfo: Array(
-			"Page Info :\n",
-			"-----------\n",
-			`href\t\t: ${document['location']['href']}\n`,
-			`protocol\t: ${document['location']['protocol']}\n`,
-			`hostname\t: ${document['location']['hostname']}\n`,
-			`pathname\t: ${document['location']['pathname']}\n`,
-			`hash\t\t: ${document['location']['hash']}\n`,
-			`title\t\t: ${document['title']}\n`,
-			`author\t\t: ${document['author']}\n`,
-			`author (alias)\t: ${document['authorAlias']}\n`,
-			`last modified\t: ${document['lastModified']}\n`
-		)
-	}
-
-	for(let i = 0; i < content.msg.length; i++)
-		output += content.msg[i];
-
-	for(let i = 0; i < content.pageInfo.length; i++)
-		output += content.pageInfo[i];
-
-	console.info(output);
 }
 
 function skillsBar(skill) {
@@ -294,6 +252,6 @@ $(document).ready(function() {
 	credit();
 });
 
-/**********
-*   END   *
-**********/
+/**
+ * END
+ */
