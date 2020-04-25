f601470b41712a78024eeb67ce11fc9a2b2aa45b
tracks12 <anarchy.dark97@gmail.com>
2020-04-25 23:51:18 +0200

    Adding doc comment in service module

--- core/services.php
+++ core/services.php
@@ -8,18 +8,33 @@
 	define('AES_256_CBC', 'aes-256-cbc');
 
 	class services {
+		/**
+		 * verify if mail is valid
+		 * @param string $data string of mail
+		 * @return bool [true/false]
+		 */
 		public static function isMail($data) {
 			$return = filter_var($data, FILTER_VALIDATE_EMAIL);
 
 			return $return;
 		}
 
+		/**
+		 * verify if number is phone number
+		 * @param string $data string of phone number
+		 * @return bool [true/false]
+		 */
 		public static function isPhone($data) {
 			$return = preg_match("/^[0-9 ]*$/", $data);
 
 			return $return;
 		}
 
+		/**
+		 * check input parameter
+		 * @param string $data string of input parameter
+		 * @return string string of output and verify parameter
+		 */
 		public static function isInput($data) {
 			$data = trim($data);
 			$data = stripslashes($data);
@@ -28,6 +43,10 @@
 			return $return;
 		}
 
+		/**
+		 * generate an aes256 key encoded in base64
+		 * @return array string of aes256 key & vector in base64
+		 */
 		public static function aesGenKey() {
 			$return = [
 				'key'    => base64_encode(openssl_random_pseudo_bytes(64)),
@@ -37,6 +56,12 @@
 			return $return;
 		}
 
+		/**
+		 * encrypt data with aes256 key & vector
+		 * @param string $data data to encrypt
+		 * @param array $aes aes256 key & vector
+		 * @return string data encrypted in aes256 and encode in base64
+		 */
 		public static function aesEncrypt($data, $aes) {
 			$aes = [
 				'key'			=> base64_decode($aes['key']),
@@ -48,6 +73,12 @@
 			return $return;
 		}
 
+		/**
+		 * decrypt data with aes256 key & vector
+		 * @param string $data string of encrypted data
+		 * @param array $aes aes256 key & vector
+		 * @return string data decrypted in aes256
+		 */
 		public static function aesDecrypt($data, $aes) {
 			$aes = [
 				'key'			=> base64_decode($aes['key']),
